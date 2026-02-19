443972a0374a560a63957138cc3a3e40182ed4b1
Girish Zarbade <girishzarbade26@gmail.com>
2026-02-19 23:50:33 +0000

    google_search_agent

---
+++ apps/02_google_agent.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langchain_groq import ChatGroq
+from langchain.agents import create_agent
+
+llm = ChatGroq(model="openai/gpt-oss-20b")
+search = GoogleSerperAPIWrapper()
+
+agent = create_agent(
+    model=llm,
+    tools=[search.run],
+    system_prompt="You are a agents and can search any question on google"
+)
+
+while True:
+    query = input("User: ")
+    if query.lower()== "quit":
+        print("GoodByee!")
+        break
+    
+    response= agent.invoke({"messages":[{"role":"user", "content":query}]})
+    print("AI: ", response["messages"][-1].content)
